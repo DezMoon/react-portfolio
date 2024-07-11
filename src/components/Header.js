09fb7b0290dcd2db055ad69dc6909b7277916844
Desmond Moonga <moongachiku@gmail.com>
2024-07-11 11:06:19 +0200

    Eddited Banner

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,23 @@
-import React from 'react';
+import React from "react";
 // Images
-import Logo from '../assets/logo.svg'
+import Logo from "../assets/logo.png";
 
 const Header = () => {
-	return (
-		<header className="py-8">
-			<div className="container mx-auto">
-				<div className="flex justify-between items-center">
-					{/* Logo */}
-					<a href="#home">
-						<img src={Logo} alt="" />
-					</a>
+  return (
+    <header className="py-8">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          {/* Logo */}
+          <a href="#home">
+            <img src={Logo} alt="" className="logo-main" />
+          </a>
 
-					{/* Button */}
-					<button className="btn btn-sm">Work with me</button>
-				</div>
-			</div>
-		</header>
-	);
+          {/* Button */}
+          <button className="btn btn-sm">Work with me</button>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
